922101981af13cf77c0d2b4f495398454d74768f
ericgurevich <tug00319@temple.edu>
2019-11-20 14:17:41 -0500

    Configured init.py, intent, and entity to allow for querying a specific team score and getting output

--- __init__.py
+++ __init__.py
@@ -2,12 +2,31 @@ from mycroft import MycroftSkill, intent_file_handler
 
 
 class NbaScoreboard(MycroftSkill):
+    teamIDs = None
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def initialize(self):
+        self.register_entity_file('team.entity')
+
+        #match team names to api team IDs
+        teamIDs = {
+                'sixers': 0,
+                'seventy sixers': 0
+        }
+
     @intent_file_handler('scoreboard.nba.intent')
     def handle_scoreboard_nba(self, message):
-        self.speak_dialog('scoreboard.nba')
+        team = message.data.get('team')
+
+        #fill in score from api
+        score = 50
+        
+        if team:
+            self.speak("The score for the most recent " + team + " game is " + str(score) + ".")
+        else:
+            self.speak("Team not found.")
 
 
 def create_skill():
